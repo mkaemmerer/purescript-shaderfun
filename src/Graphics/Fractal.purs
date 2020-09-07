b0b84bd91ebfcbe8a1d65c375bb051a16658167c
Matt Kaemmerer <matthew.kaemmerer@gmail.com>
2020-09-07 18:34:48 +0900

    Use rec instead of recM for fractal functions

--- src/Graphics/Fractal.purs
+++ src/Graphics/Fractal.purs
@@ -4,13 +4,14 @@ import Prelude
 
 import Control.Monad.Rec.Class (class MonadRec, Step(..), tailRec, tailRecM2)
 import Data.Complex (Complex)
+import Data.Either (Either)
 import Data.Int (toNumber)
 import Data.Tuple (Tuple(..))
 import Data.Vec2 (Vec2)
 import Data.VectorSpace (magnitudeSquared)
-import Shader.Expr (Expr, gt, ifE, log2, num, tuple, vec2ToComplex)
+import Shader.Expr (Expr, done, gt, ifE, log2, loop, num, tuple, vec2ToComplex)
 import Shader.Expr.Cast (cast, from)
-import Shader.ExprBuilder (type (|>), decl)
+import Shader.ExprBuilder (type (|>), decl, rec)
 
 type Point = Complex
 type IterCount = Number
@@ -30,14 +31,15 @@ repeatC n f = tailRec go { acc: identity, count: n }
   go { acc, count: 0 } = Done acc
   go { acc, count }    = Loop { acc: f >>> acc, count: count - 1}
 
-orbitStep :: Expr Point -> (Tuple Point IterCount) |> (Tuple Point IterCount)
+
+orbitStep :: Expr Point -> (Tuple Point IterCount) |> (Either (Tuple Point IterCount) (Tuple Point IterCount))
 orbitStep c zt = do
   let (Tuple z t) = from zt
   q <- decl $ (z * z) + c
   esc <- decl $ magnitudeSquared q `gt` num 4.0
-  z' <- decl $ ifE esc z q
-  t' <- decl $ ifE esc t (t + one)
-  pure (tuple z' t')
+  pure $ ifE esc
+    (done (tuple z t))
+    (loop (tuple q (t + one)))
 
 -- | `juliaSet n c` approximates a julia set centered at `c` by iterating its orbit `n` times.
 juliaSet :: Int -> Complex -> (Vec2 |> Number)
@@ -45,7 +47,7 @@ juliaSet n c z = do
   let c' = cast c
   let n' = num $ toNumber n
   -- (point, iteration count)
-  (Tuple z' t') <- from <$> repeatM n (orbitStep c') (tuple (vec2ToComplex z) zero)
+  (Tuple z' t') <- from <$> rec n (orbitStep c') (tuple (vec2ToComplex z) zero)
   -- Smooth iteration count
   d <- decl $ t' - (log2 $ log2 $ magnitudeSquared $ z')
   f <- decl $ d / n'
@@ -57,7 +59,7 @@ mandelbrotSet n c = do
   let c' = vec2ToComplex c
   let n' = num $ toNumber n
   -- (point, iteration count)
-  (Tuple z' t') <- from <$> repeatM n (orbitStep c') (tuple zero zero)
+  (Tuple z' t') <- from <$> rec n (orbitStep c') (tuple zero zero)
   -- Smooth iteration count
   d <- decl $ t' - (log2 $ log2 $ magnitudeSquared $ z')
   f <- decl $ d / n'
