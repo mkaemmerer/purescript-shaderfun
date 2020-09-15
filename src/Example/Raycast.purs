38450a43853d3bc4ef79437b73f7e557d60db0f9
Matt Kaemmerer <matthew.kaemmerer@gmail.com>
2020-09-15 14:50:28 +0900

    Some refactors and a torus primitive

--- src/Example/Raycast.purs
+++ src/Example/Raycast.purs
@@ -6,27 +6,33 @@ import Data.Color (Color)
 import Data.Vec2 (Vec2)
 import Data.Vec3 (Vec3(..))
 import Data.VectorSpace (dot, (*^), (^+^))
-import Graphics.Camera (perspectiveCam)
+import Graphics.Camera (Ray, perspectiveCam)
 import Graphics.ColorRamp (grayscaleRamp)
 import Graphics.DomainTransform (scale, translate)
-import Graphics.SDF3 (SDF3, raymarch, sphere, surfaceNormal)
-import Shader.Expr (fst, snd, vec3)
-import Shader.ExprBuilder (decl)
+import Graphics.SDF3 (SDF3, raymarch, surfaceNormal, torus)
+import Shader.Expr (fst, num, snd, vec3)
+import Shader.ExprBuilder (type (|>), decl)
 import Shader.ExprFunction (ShaderProgram, runShaderProgram, shaderProgram)
 import Shader.GLSL (toGLSL)
 
-sdf :: SDF3
-sdf = translate (Vec3 {x: 0.0, y: 3.0, z: 0.0}) $ sphere 2.0
-
 program :: ShaderProgram Vec2 Color
-program = shaderProgram $ scale 500.0 $ perspectiveCam >=> render >=> grayscaleRamp
+program = shaderProgram $ scale 500.0 $ perspectiveCam >=> renderLighting sdf >=> grayscaleRamp
   where
-    render ray = do
-      dist   <- raymarch sdf ray
-      point  <- decl $ (fst ray) ^+^ dist *^ (snd ray)
-      normal <- surfaceNormal sdf point
-      let light = normal `dot` (vec3 zero zero one)
-      pure light
+    sdf :: SDF3
+    sdf = translate (Vec3 {x: 0.0, y: 3.0, z: 0.0}) $ torus 2.0 0.5
+
+renderDepth :: SDF3 -> Ray |> Number
+renderDepth sdf ray = do
+  dist <- raymarch sdf ray
+  pure $ dist - num 0.5
+
+renderLighting :: SDF3 -> Ray |> Number
+renderLighting sdf ray = do
+  dist   <- raymarch sdf ray
+  point  <- decl $ (fst ray) ^+^ dist *^ (snd ray)
+  normal <- surfaceNormal sdf point
+  let light = normal `dot` (vec3 zero zero one)
+  pure light
 
 source :: String
 source = toGLSL $ runShaderProgram program
--- src/Graphics/SDF/SDF3.purs
+++ src/Graphics/SDF/SDF3.purs
@@ -2,6 +2,7 @@ module Graphics.SDF3
   ( SDF3
   , sphere
   , box
+  , torus
   , raymarch
   , surfaceNormal
   ) where
@@ -13,7 +14,7 @@ import Data.Tuple (Tuple(..))
 import Data.Vec3 (Vec3)
 import Data.VectorSpace ((*^), (^+^), (^-^), (^/))
 import Graphics.SDF (circle)
-import Shader.Expr (Expr, abs, done, ifE, length, loop, lte, max, min, num, projX, projY, projZ, snd, tuple, vec3)
+import Shader.Expr (Expr, abs, done, ifE, length, loop, lte, max, min, num, projX, projY, projZ, snd, tuple, vec2, vec3)
 import Shader.Expr.Cast (cast, from)
 import Shader.ExprBuilder (type (|>), decl, rec)
 
@@ -25,6 +26,13 @@ type SDF3 = Vec3 |> Number
 sphere :: Number -> SDF3
 sphere = circle
 
+torus :: Number -> Number -> SDF3
+torus r1 r2 p = do
+  h <- decl $ vec2 (projX p) (projY p)
+  v <- decl $ vec2 (length h - cast r1) (projZ p)
+  d <- decl $ length v - (cast r2)
+  pure d
+
 -- | `box v` is a rectangle centered at the origin with a corner at `v`
 box :: Vec3 -> SDF3
 box corner p = do
@@ -32,10 +40,13 @@ box corner p = do
   let d' = from d
   c <- decl $ vec3 (max d'.x zero) (max d'.y zero) (max d'.z zero)
   m <- decl $ (min (max d'.x (max d'.y d'.z)) zero)
-  pure $ (length c) + m
+  pure $ length c + m
 
 absV :: Expr Vec3 -> Expr Vec3
 absV v = vec3 (abs $ projX v) (abs $ projY v) (abs $ projZ v)
+
+
+-- Ray Marching
 
 type Pos = Vec3
 type Ray = Tuple Pos Vec3
@@ -43,6 +54,9 @@ type Dist = Number
 
 eps :: Expr Number
 eps = num 0.0001
+
+normalized :: Expr Vec3 -> Expr Vec3
+normalized v = v ^/ length v
 
 raymarch :: SDF3 -> Ray |> Dist
 raymarch sdf ray = snd <$> rec 256 march (tuple p0 zero)
@@ -57,19 +71,15 @@ raymarch sdf ray = snd <$> rec 256 march (tuple p0 zero)
       (done (tuple p' t))
       (loop (tuple p' (t + dist)))
 
-
-normalized :: Expr Vec3 -> Expr Vec3
-normalized v = v ^/ length v
-
 -- Numeric surface normals via central differences
 surfaceNormal :: SDF3 -> Vec3 |> Vec3
 surfaceNormal sdf v = do
   let i = vec3 one zero zero
   let j = vec3 zero one zero
   let k = vec3 zero zero one
-  dx  <- sdf (v ^+^ eps *^ i)  `lift2 (-)`  sdf (v ^-^ eps *^ i)
-  dy  <- sdf (v ^+^ eps *^ j)  `lift2 (-)`  sdf (v ^-^ eps *^ j)
-  dz  <- sdf (v ^+^ eps *^ k)  `lift2 (-)`  sdf (v ^-^ eps *^ k)
+  dx <- sdf (v ^+^ eps *^ i)  `lift2 (-)`  sdf (v ^-^ eps *^ i)
+  dy <- sdf (v ^+^ eps *^ j)  `lift2 (-)`  sdf (v ^-^ eps *^ j)
+  dz <- sdf (v ^+^ eps *^ k)  `lift2 (-)`  sdf (v ^-^ eps *^ k)
   n  <- decl $ vec3 dx dy dz
   n' <- decl $ normalized n
   pure n'
