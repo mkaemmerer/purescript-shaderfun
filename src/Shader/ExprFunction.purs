a67612b82220d4cc9e7869400953994189e57d92
Matt Kaemmerer <matthew.kaemmerer@gmail.com>
2020-09-25 14:00:54 +0900

    Simplify expr function with newtype wrapper

--- src/Shader/ExprFunction.purs
+++ src/Shader/ExprFunction.purs
@@ -11,40 +11,30 @@ import Prelude
 import Control.Apply (lift2)
 import Control.Bind (composeKleisliFlipped)
 import Data.Color (Color)
+import Data.Newtype (class Newtype, over, unwrap, wrap)
 import Data.Vec2 (Vec2)
 import Data.VectorSpace (class AdditiveGroup, class InnerSpace, class VectorSpace, addV, dot, negateV, scale, subV, zeroV)
 import Shader.Expr (Expr, p)
 import Shader.ExprBuilder (Builder, runExprBuilder)
 
-type BFunc a b = a -> Builder b
-data BuilderFunc a b = BuilderFunc (BFunc a b)
-type ShaderProgram a b = BuilderFunc (Expr a) (Expr b)
+newtype BuilderFunc a b = BuilderFunc (a -> Builder b)
+derive instance newtypeBuilderFunc :: Newtype (BuilderFunc a b) _
 
+type ShaderProgram a b = BuilderFunc (Expr a) (Expr b)
 infixr 4 type ShaderProgram as :>
 
-shaderProgram :: forall a b. BFunc (Expr a) (Expr b) -> ShaderProgram a b
-shaderProgram = wrapBuilder
+shaderProgram :: forall a b. ((Expr a) -> Builder (Expr b)) -> ShaderProgram a b
+shaderProgram = wrap
 
-wrapBuilder :: forall a b. BFunc a b -> BuilderFunc a b
-wrapBuilder f = BuilderFunc f
-
-unwrapBuilder :: forall a b. BuilderFunc a b -> (BFunc a b)
-unwrapBuilder (BuilderFunc f) = f
-
-over ::
-  forall a b c d.
-  (BFunc a b -> BFunc c d) ->
-  (BuilderFunc a b -> BuilderFunc c d)
-over f x = wrapBuilder $ f (unwrapBuilder x)
-
+type BFunc a b = a -> Builder b
 over2 ::
   forall a b c d e f.
   (BFunc a b -> BFunc c d -> BFunc e f) ->
   (BuilderFunc a b -> BuilderFunc c d -> BuilderFunc e f)
-over2 f x y = wrapBuilder $ f (unwrapBuilder x) (unwrapBuilder y)
+over2 f x y = wrap $ f (unwrap x) (unwrap y)
 
 runShaderProgram :: ShaderProgram Vec2 Color -> Expr Color
-runShaderProgram program = runExprBuilder (unwrapBuilder program $ p)
+runShaderProgram program = runExprBuilder (unwrap program $ p)
 
 
 -- Function type class instances
@@ -52,16 +42,16 @@ instance semigroupoidBuilderFunc :: Semigroupoid BuilderFunc where
   compose = over2 composeKleisliFlipped
 
 instance categoryBuilderFunc :: Category BuilderFunc where
-  identity = wrapBuilder $ identity >>> pure
+  identity = wrap $ identity >>> pure
 
 instance functorBuilderFunc :: Functor (BuilderFunc a) where
-  map f = over $ map (map f)
+  map f = over BuilderFunc $ map (map f)
 
 instance applyBuilderFunc :: Apply (BuilderFunc a) where
   apply = over2 $ \f g x -> apply (f x) (g x)
 
 instance applicativeBuilderFunc :: Applicative (BuilderFunc a) where
-  pure x = wrapBuilder $ pure $ pure x
+  pure x = wrap $ pure $ pure x
 
 
 -- Numeric type class instances
